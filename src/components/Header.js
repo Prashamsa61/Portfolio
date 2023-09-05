095b29322eaece3b6ee29566e7f6c949a5c5947f
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-05 19:19:52 +0545

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+
 const Header = () => {
-  return <div className=" bg-pink-300" id="home"> Header</div>;
+  return <header className="  py-8">
+    
+   <div className="container mx-auto">
+    <div className="flex justify-between items-center">
+      <a href="#">
+      <button className="text-2xl italic underline ">Tech Enthusiast</button>
+      </a>
+      <button className="btn btn-sm">Work With Me</button>
+    </div>
+   </div>
+   </header>;
 };
 
 export default Header;
