8f51c55e7f05080d22307916f4c85a5101f44fd8
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-06 18:37:46 +0545

    changes in header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,9 +8,9 @@ const Nav = () => {
     <nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
       <div className="container mx-auto ">
         <div
-          className="w-full bg-black/20 backdroup-blur-2xl 
+          className="w-full bg-black backdroup-blur-2xl 
         rounded-full max-w-[460px] mx-auto px-6 flex justify-between 
-        text-2xl text-white/50 "
+        text-2xl text-white "
         >
           <Link
             to="home"
@@ -18,7 +18,7 @@ const Nav = () => {
             smooth={true}
             spy={true}
             offset={-200}
-            className="cursor-pointer w-[60px] h-[60px] flex items-center
+            className="cursor-pointer w-[60px] h-[60px] flex items-center 
            justify-center"
           >
             <BiHomeAlt />
