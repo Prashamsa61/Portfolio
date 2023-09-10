39e5e5f97d8c67d35bd1f42c2977d3b0a4da87ad
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-10 21:27:13 +0545

    mapping

--- src/components/Services.js
+++ src/components/Services.js
@@ -51,7 +51,30 @@ const Services = () => {
           <div>
             {/* services list */}
             <div>
-              {services.map((services,index)={})}
+              {services.map((services, index) => {
+                //destructive service
+                const { name, description, link } = service;
+                return (
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px]
+                  flex"
+                    key={index}
+                  >
+                    <div className="max-w-[476px]">
+                      <h4
+                        className="text-[20px] tracking-wider font-primary
+                      font-semibold mb-6"
+                      >
+                        {name}
+                      </h4>
+                      <p className="font-secondary leading-tight">
+                        {description}
+                      </p>
+                    </div>
+                    <div>{link}</div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
