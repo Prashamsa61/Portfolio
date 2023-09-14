a069c0337695c6bc563ebc6f4af790d8e54afbb7
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-14 21:53:34 +0545

    work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div className='section' id="work">Work</div>;
+  return (
+    <section className="section" id="work">
+      <div className="cotainer mx-auto">
+        <div>
+          <div>
+            {/* text */}
+            <div>
+              <h2 className="h2 leading-tight  text-accent">
+                My latest <br />
+                work
+              </h2>
+              <p className="max-w-sm mb-16"></p>
+            </div>
+            {/* image */}
+
+            <div>image</div>
+          </div>
+          <div> 2</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;
