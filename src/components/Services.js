d410e47ab177f03406a9e2e88dc37a304b8696d3
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-14 21:47:08 +0545

    services complete

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { bsArrowUpRight } from "react-icons/bs";
+import { BsArrowUpRight } from "react-icons/bs";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
@@ -23,21 +22,20 @@ const services = [
       "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
     link: "Learn More",
   },
-  {
-    name: "UI & UX Design",
-    description:
-      "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    link: "Learn More",
-  },
 ];
+
 const Services = () => {
   return (
     <section className="section" id="services">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row">
           {/*text  $ {image}*/}
-          <div
-            className="flex-1 bg:services  lg:bg-bottom
+          <motion.div
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex-1 bg:services lg:bg-bottom
            bg-no-repeat mix-blend-lighten mb-12 lg:mb-0"
           >
             <h2 className="h2 text-accent mb-6">what i do</h2>
@@ -46,33 +44,43 @@ const Services = () => {
               Vokalia and Consonantia, there live the blind texts.
             </h3>
             <button className="btn btn-sm">See my work</button>
-          </div>
+          </motion.div>
           {/* services */}
-          <div>
+          <div className="flex-1">
             {/* services list */}
             <div>
-              {services.map((services, index) => {
-                //destructive service
+              {services.map((service, index) => {
+                // Destructure service
                 const { name, description, link } = service;
                 return (
-                  <div
-                    className="border-b border-white/20 h-[146px] mb-[38px]
-                  flex"
+                  <motion.div
+                    variants={fadeIn("left", 0.5)}
+                    initial="hidden"
+                    whileInView={"show"}
+                    viewport={{ once: false, amount: 0.7 }}
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
                     key={index}
                   >
                     <div className="max-w-[476px]">
-                      <h4
-                        className="text-[20px] tracking-wider font-primary
-                      font-semibold mb-6"
-                      >
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
                         {name}
                       </h4>
                       <p className="font-secondary leading-tight">
                         {description}
                       </p>
                     </div>
-                    <div>{link}</div>
-                  </div>
+                    <div className="flex felx-col flex-1 items-end ">
+                      <a
+                        href="#"
+                        className="btn w-9 h-9 mb-[42px] justify-center items-center"
+                      >
+                        <BsArrowUpRight />
+                      </a>
+                      <a href="#" className="text-gradient text-sm">
+                        {link}
+                      </a>
+                    </div>
+                  </motion.div>
                 );
               })}
             </div>
