bc7018f9199b4f01460ad00042e593f271d9c91e
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-19 06:40:18 +0545

    work page update

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,12 +8,12 @@ import Img3 from "../assets/portfolio-img3.png";
 const Work = () => {
   return (
     <section className="section" id="work">
-      <div className="cotainer mx-auto">
-        <div className="flex felx-col lg:flex-row  gap-x-10 ">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-12">
+          {/* Portfolio Item 1 */}
           <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
-            {/* text */}
             <div>
-              <h2 className="h2 leading-tight  text-accent">
+              <h2 className="h2 leading-tight text-accent">
                 My latest <br />
                 work
               </h2>
@@ -25,44 +25,44 @@ const Work = () => {
                 cursus, purus ut varius tincidunt, tortor felis venenatis justo,
                 id dictum felis ex in libero.
               </p>
-              <button className="btn btn-sm ">VIew all projects</button>
+              <button className="btn btn-sm">View all projects</button>
             </div>
-            {/* image */}
 
-            <div
-              className="group relative overflow-hidden border-2
-            border-white/50 rounded-xl"
-            >
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               {/* overlay */}
-              <div
-                className="group-hover:bg-black/70 w-full h-full absolute 
-              z-40 transition-all duration-300"
-              ></div>
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               {/* img */}
-              <img
-                src={Img1}
-                alt=""
-                className="group-hover:scale-125 transition-all duration-500"
-              />
+              <img src={Img1} alt="" className="group-hover:scale-125 transition-all duration-500" />
               {/* pretitle */}
-              <div
-                className="absolute -bottom-full left-12
-              group-hover:bottom-24 tranistion:all duration-500 z-50
-              "
-              >
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
                 <span className="text-gradient">Ui/ux design</span>
               </div>
               {/* title */}
-              <div
-                className="absolute -bottom-full left-12
-              group-hover:bottom-14 tranistion:all duration-700 z-50
-              "
-              >
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
                 <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
           </div>
-          <div className="flex-1 "> 2</div>
+
+          {/* Portfolio Items 2 and 3 */}
+          <div className="flex-1 flex flex-col gap-y-12">
+            {[Img2, Img3].map((imgSrc, index) => (
+              <div key={index} className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                {/* img */}
+                <img src={imgSrc} alt="" className="group-hover:scale-125 transition-all duration-500" />
+                {/* pretitle */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                  <span className="text-gradient">Ui/ux design</span>
+                </div>
+                {/* title */}
+                <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                  <span className="text-3xl text-white">Project Title</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </section>
