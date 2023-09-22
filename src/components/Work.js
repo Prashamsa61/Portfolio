19021e29587c1607c4300808f204474ca6c25d95
Prashamsa Rijal <prashamsarijal61@gmail.com>
2023-09-22 10:53:59 +0545

    work updated

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 import Img1 from "../assets/portfolio-img1.png";
@@ -6,86 +6,119 @@ import Img2 from "../assets/portfolio-img2.png";
 import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
+  const portfolioItems = [
+    {
+      imgSrc: Img1,
+      pretitle: "UI/UX Design",
+      title: "Project Title 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    },
+    {
+      imgSrc: Img2,
+      pretitle: "UI/UX Design",
+      title: "Project Title 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    },
+    {
+      imgSrc: Img3,
+      pretitle: "UI/UX Design",
+      title: "Project Title 3",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    },
+  ];
+
+  const [activeItem, setActiveItem] = useState(0);
+
+  const handleItemClick = (index) => {
+    setActiveItem(index);
+  };
+
   return (
     <section className="section" id="work">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row gap-x-10">
-          {/* Portfolio Item 1 */}
-          <motion.div
-            variants={fadeIn("left", 0.5)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
-          >
-            <div>
+          <div className="lg:w-1/2 flex flex-col gap-y-12 mb-10 lg:mb-0">
+            <motion.div
+              variants={fadeIn("left", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.3 }}
+            >
               <h2 className="h2 leading-tight text-accent">
                 My latest <br />
                 work
               </h2>
               <p className="max-w-sm mb-16">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                facilisi. Sed euismod, justo nec cursus interdum, sapien justo
-                consectetur nisi, nec lacinia mi sapien eget risus. Fusce luctus
-                mi nec nisl ultrices, a ultricies justo venenatis. Aenean
-                cursus, purus ut varius tincidunt, tortor felis venenatis justo,
-                id dictum felis ex in libero.
+                {portfolioItems[activeItem].description}
               </p>
               <button className="btn btn-sm">View all projects</button>
-            </div>
+            </motion.div>
+          </div>
 
-            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
-              {/* overlay */}
-              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-              {/* img */}
-              <img
-                src={Img1}
-                alt=""
-                className="group-hover:scale-125 transition-all duration-500"
-              />
-              {/* pretitle */}
-              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
-                <span className="text-gradient">Ui/ux design</span>
-              </div>
-              {/* title */}
-              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
-                <span className="text-3xl text-white">Project Title</span>
-              </div>
-            </div>
-          </motion.div>
-
-          {/* Portfolio Items 2 and 3 */}
-          <motion.div
-            variants={fadeIn("right", 0.5)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.3 }}
-            className="flex-1 flex flex-col gap-y-12"
-          >
-            {[Img2, Img3].map((imgSrc, index) => (
-              <div
+          <div className="lg:w-1/2 flex flex-col gap-y-12">
+            {portfolioItems.slice(0, 2).map((item, index) => (
+              <motion.div
                 key={index}
-                className="group relative overflow-hidden border-2 border-white/50 rounded-xl"
+                variants={fadeIn("left", 0.5)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="lg:flex"
               >
-                {/* overlay */}
-                <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-                {/* img */}
-                <img
-                  src={imgSrc}
-                  alt=""
-                  className="group-hover:scale-125 transition-all duration-500"
-                />
-                {/* pretitle */}
-                <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
-                  <span className="text-gradient">Ui/ux design</span>
+                <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                  {/* overlay */}
+                  <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                  {/* img */}
+                  <img
+                    src={item.imgSrc}
+                    alt=""
+                    className="group-hover:scale-125 transition-all duration-500"
+                  />
+                  {/* pretitle */}
+                  <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                    <span className="text-gradient">{item.pretitle}</span>
+                  </div>
+                  {/* title */}
+                  <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                    <span className="text-3xl text-white">{item.title}</span>
+                  </div>
                 </div>
-                {/* title */}
-                <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
-                  <span className="text-3xl text-white">Project Title</span>
-                </div>
-              </div>
+              </motion.div>
             ))}
-          </motion.div>
+
+            {/* Third Image */}
+            <div className="lg:flex">
+              <motion.div
+                variants={fadeIn("left", 0.5)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+              >
+                <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                  {/* overlay */}
+                  <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                  {/* img */}
+                  <img
+                    src={portfolioItems[2].imgSrc}
+                    alt=""
+                    className="group-hover:scale-125 transition-all duration-500"
+                  />
+                  {/* pretitle */}
+                  <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                    <span className="text-gradient">
+                      {portfolioItems[2].pretitle}
+                    </span>
+                  </div>
+                  {/* title */}
+                  <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                    <span className="text-3xl text-white">
+                      {portfolioItems[2].title}
+                    </span>
+                  </div>
+                </div>
+              </motion.div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
